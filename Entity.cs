81b118285876456c58ab6f0648ab841ec2ff26e3
wiktorkuziola <wiktorkuziola.mail@gmail.com>
2026-05-14 19:52:54 +0200

    Update Entity.cs

--- Entity.cs
+++ Entity.cs
@@ -2,7 +2,10 @@ class Entity {
     public int x { get; set; }
     public int y { get; set; }
     public virtual Tile tile => Tile.Entity;
+    public virtual void Update(Board board)
+    {
 
+    }
     public Entity(int x, int y) {
         this.x = x;
         this.y = y;
