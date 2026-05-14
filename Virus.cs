576bec31286a15bdbcba402d8eee96e4df7d58f0
wiktorkuziola <wiktorkuziola.mail@gmail.com>
2026-05-14 19:47:34 +0200

    Create Virus.cs

---
+++ Virus.cs
@@ -0,0 +1,57 @@
+class Virus
+{
+    public string Name { get; set; }
+
+    public float Infectivity { get; set; }
+
+    public float Mortality { get; set; }
+
+    public int InfectionRange { get; set; }
+
+    Random rng = new();
+
+    public Virus(
+        string name,
+        float infectivity,
+        float mortality,
+        int infectionRange)
+    {
+        Name = name;
+        Infectivity = infectivity;
+        Mortality = mortality;
+        InfectionRange = infectionRange;
+    }
+
+    public void Spread(Board board)
+    {
+        var infected = board.GetInfectedHumans();
+
+        foreach (var source in infected)
+        {
+            var nearby =
+                board.GetHumansInRange(
+                    source.x,
+                    source.y,
+                    InfectionRange);
+
+            foreach (var target in nearby)
+            {
+                if (target == source)
+                    continue;
+
+                if (target.IsInfected)
+                    continue;
+
+                if (rng.NextSingle() <= Infectivity)
+                {
+                    target.Infect(this);
+                }
+            }
+
+            if (rng.NextSingle() <= Mortality)
+            {
+                source.Die();
+            }
+        }
+    }
+}
