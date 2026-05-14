8cada602ad282544e1b49da419ec12198b2cd394
wiktorkuziola <wiktorkuziola.mail@gmail.com>
2026-05-14 19:54:35 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,58 @@
+using System.Linq;
 Board board = new Board(40, 40);
 
-// board.addEntity(new Human(1, 2));
-// board.addEntity(new Human(2, 3));
+Virus virus = new Virus(
+    "Covid",
+    0.2f,
+    0.01f,
+    2);
 
-board.print();
+// spawn humans
+for (int i = 0; i < 50; i++)
+{
+    board.addRandomEntity((x, y) =>
+        new Human(x, y));
+}
+
+// spawn doctors
+for (int i = 0; i < 3; i++)
+{
+    board.addRandomEntity((x, y) =>
+        new Doctor(x, y));
+}
+
+// spawn hospital
+board.addRandomEntity((x, y) =>
+    new Hospital(x, y));
+
+// infect first human
+var firstHuman =
+    board.GetHumansInRange(0, 0, 999)
+        .First();
+
+firstHuman.Infect(virus);
+
+// simulation loop
+while (true)
+{
+    Console.Clear();
+
+    foreach (var entity in board.entities)
+    {
+        if (entity is Human human)
+        {
+            human.Update(board);
+        }
+
+        if (entity is Hospital hospital)
+        {
+            hospital.Update(board);
+        }
+    }
+
+    virus.Spread(board);
+
+    board.print();
+
+    Thread.Sleep(200);
+}
