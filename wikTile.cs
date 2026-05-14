d9b4ef96465ab88162153dba06869936e1c72d31
wiktorkuziola <wiktorkuziola.mail@gmail.com>
2026-05-14 19:47:15 +0200

    Create wikTile.cs

---
+++ wikTile.cs
@@ -0,0 +1,18 @@
+public enum Tile {
+    Water = ',',
+    Land = '.',
+    Entity = 'E',
+    Human = 'H',
+    Doctor = 'D',
+    Hospital = '+',
+    Region0 = '0',
+    Region1 = '1',
+    Region2 = '2',
+    Region3 = '3'
+}
+
+public static class TileExtensions {
+    public static char Symbol(this Tile tile) {
+        return (char)tile;
+    }
+}
