91425d8636d4c43a84f198f29e83bc48736115fc
wiktorkuziola <wiktorkuziola.mail@gmail.com>
2026-05-26 22:27:16 +0200

    Update Human.cs

--- Models/Human.cs
+++ Models/Human.cs
@@ -13,21 +13,48 @@ public class Human : Entity, ITile
     public bool IsInfected => Virus != null;
 
     public int HealingTicks { get; set; }
+    
+    public Tile HomeRegion { get; set; }
+
+    public int MigrationCooldown { get; set; }
 
     Random rng = new();
 
     public Human(int x, int y) : base(x, y)
     {
-
+        HomeRegion = Tile.Region0;
     }
 
     public override void Update(Board board)
     {
+        if (!IsAlive)
+            return;
+
         Move(board);
     }
 
     public virtual void Move(Board board)
     {
+        float movementChance =
+            1f - board.AwarenessLevel;
+
+        if (rng.NextSingle() > movementChance)
+        {
+            return;
+        }
+        
+        if (MigrationCooldown > 0)
+        {
+            MigrationCooldown--;
+        }
+        
+        if (rng.NextSingle() < 0.01f &&
+            MigrationCooldown == 0)
+        {
+            LongDistanceMigration(board);
+            return;
+        }
+        
         int dx = rng.Next(-1, 2);
         int dy = rng.Next(-1, 2);
 
@@ -36,12 +63,54 @@ public class Human : Entity, ITile
 
         if (board.IsWalkable(nx, ny))
         {
+            if (board.LockdownEnabled)
+            {
+                Tile currentRegion =
+                    board.GetRegionAt(x, y);
+
+                Tile nextRegion =
+                    board.GetRegionAt(nx, ny);
+
+                // nie można opuścić regionu
+                if (currentRegion != nextRegion)
+                {
+                    return;
+                }
+            }
+
             x = nx;
             y = ny;
         }
     }
+    
+    private void LongDistanceMigration(Board board)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            int nx = rng.Next(board.Width);
+            int ny = rng.Next(board.Height);
 
-    public void Infect(Virus virus)
+            if (!board.IsWalkable(nx, ny))
+                continue;
+
+            Tile targetRegion =
+                board.GetRegionAt(nx, ny);
+            
+            if (targetRegion != HomeRegion)
+            {
+                x = nx;
+                y = ny;
+                
+                HomeRegion = targetRegion;
+                
+                MigrationCooldown = 50;
+
+                break;
+            }
+        }
+    }
+
+    public virtual void Infect(Virus virus)
     {
         if (Virus != null)
             return;
@@ -58,5 +127,7 @@ public class Human : Entity, ITile
     public void Die()
     {
         IsAlive = false;
+
+        Virus = null;
     }
 }
