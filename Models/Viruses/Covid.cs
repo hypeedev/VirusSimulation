b4c137eb4ddf7ffd38ee36f9f1016de43f352cb0
wiktorkuziola <wiktorkuziola.mail@gmail.com>
2026-06-10 19:23:34 +0200

    Update Covid.cs

--- Models/Viruses/Covid.cs
+++ Models/Viruses/Covid.cs
@@ -2,7 +2,7 @@ public class Covid : IVirus
 {
     public string Name { get; } = "Covid";
 
-    public float Infectivity { get; } = 0.5f;
+    public float Infectivity { get; } = 0.65f;
 
     public float Mortality { get; } = 0.02f;
 
